248b064c80755f976642086eafa29b822419c516
MatGod <mat_gord@mail.ru>
2017-10-18 04:57:00 +0200

    2 init

---
+++ lab_2/change.h
@@ -0,0 +1,13 @@
+//
+// Created by matgod on 16.10.17.
+//
+
+
+
+#ifndef LAB_1_CHANGE_H
+#define LAB_1_CHANGE_H
+void printInBit(int);
+void printInBit(long double);
+int reversGroup(int x, unsigned int start_position, unsigned int count_of_bits);
+long double reversGroup(long double x, unsigned int start_position, unsigned int count_of_bits);
+#endif //LAB_1_CHANGE_H
